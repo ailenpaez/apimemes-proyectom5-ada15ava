4d893e098f511e6b308febb54f9b93fe2149d485
Ailén <128065511+ailenpaez@users.noreply.github.com>
2024-03-20 01:22:44 -0300

    Creación del middleware para la autenticación de usuarios.

---
+++ src/middleware/auth.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+import users from "../db/users.json";
+
+const validator = (req: Request, res: Response, next: NextFunction) => {
+  const { token } = req.body;
+
+  const exists = users.find((u) => u.token === token);
+
+  if (!exists) return res.json({ error: "PERMISSIONS_ARE_MISSING" });
+  next();
+};
+
+export { validator };
