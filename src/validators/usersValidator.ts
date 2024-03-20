69ad64d9214157c5a138d62fa5907766dd6413c9
Ailén <128065511+ailenpaez@users.noreply.github.com>
2024-03-20 20:28:02 -0300

    Implementación del zod en el 'userValidator'.

---
+++ src/validators/usersValidator.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+const userSchema = z.object({
+  mail: z.string(),
+  username: z.string().min(6).max(10),
+  password: z.string().min(8).max(15),
+  interests: z.array(z.string()),
+});
+
+const validateUser = (obj: any) => userSchema.safeParse(obj);
+
+const validatePartialUser = (obj: any) => userSchema.partial().safeParse(obj);
+
+export { validateUser, validatePartialUser };
